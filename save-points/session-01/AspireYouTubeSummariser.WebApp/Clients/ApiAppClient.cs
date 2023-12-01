de7272a10b666dda418a4d6b09bd5a11afcf3190
Justin Yoo <justin.yoojh@gmail.com>
2023-12-01 22:54:09 +0900

    Add save points for session 01 (#7)

---
+++ save-points/session-01/AspireYouTubeSummariser.WebApp/Clients/ApiAppClient.cs
@@ -0,0 +1,21 @@
+﻿namespace AspireYouTubeSummariser.WebApp.Clients;
+
+public interface IApiAppClient
+{
+    Task<string> SummariseAsync(string youTubeLinkUrl, string videoLanguageCode, string summaryLanguageCode);
+}
+
+public class ApiAppClient(HttpClient http) : IApiAppClient
+{
+    private readonly HttpClient _http = http ?? throw new ArgumentNullException(nameof(http));
+
+    public async Task<string> SummariseAsync(string youTubeLinkUrl, string videoLanguageCode, string summaryLanguageCode)
+    {
+        using var response = await _http.PostAsJsonAsync(
+            "summarise",
+            new { youTubeLinkUrl, videoLanguageCode, summaryLanguageCode }).ConfigureAwait(false);
+
+        var summary = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+        return summary;
+    }
+}
